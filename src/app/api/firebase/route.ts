828850eac378a4ff41076debb35121ec0c2e326c
kunchok tashi <khakontas33@gmail.com>
2025-09-13 14:15:05 -0400

    updated res

--- src/app/api/firebase/route.ts
+++ src/app/api/firebase/route.ts
@@ -1,13 +1,15 @@
 import { initializeFirebaseWithConfig } from "@/lib/firebase-admin";
-import { NextApiResponse } from "next";
 import { NextRequest } from "next/server";
 
-export async function POST(request: NextRequest, res: NextApiResponse) {
+export async function POST(request: NextRequest) {
   try {
     const body = await request.json();
     const { firebaseConfig } = body;
     if (!firebaseConfig) {
-      return res.status(400).json({ error: "Missing config" });
+      return new Response(
+        JSON.stringify({ message: "Missing firebaseConfig in request body" }),
+        { status: 400 }
+      );
     }
     const decoded = Buffer.from(firebaseConfig, "base64").toString("utf8");
 
